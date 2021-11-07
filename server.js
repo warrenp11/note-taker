39ba016a60c7b9e140905b5129776efd58e16e19
Peter <warrenp11@gmail.com>
2021-11-07 11:55:34 -0500

    api/notes POST route functioning

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// middlewear
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 const notes = require("./db/db.json");
 
+// GET routes for html pages
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
@@ -18,8 +20,16 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+// GET route for api
 app.get("/api/notes", (req, res) => {
   res.json(notes);
+});
+
+// POST route for api
+app.post("/api/notes", (req, res) => {
+    res.json(req.body);
+    console.log(req.body);
+    // create new note and send to `db.json`
 });
 
 app.listen(PORT, () => {
