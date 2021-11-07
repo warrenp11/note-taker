668218842fdf3d3f8f71365eb4d6cb4e88d2bccc
Peter <warrenp11@gmail.com>
2021-11-07 11:43:03 -0500

    api/notes GET route functional

--- server.js
+++ server.js
@@ -6,14 +6,20 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+const notes = require("./db/db.json");
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
 });
 
 app.listen(PORT, () => {
