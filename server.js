a2b1c5c03dbbb7a4fffd4743dfbe0e5e6cf4c509
Peter <warrenp11@gmail.com>
2021-11-07 11:12:18 -0500

    html GET routes functioning

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get("*/", (req, res) => {
-    res.sendFile(__dirname, "./public/index.html");
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-// app.get("/notes", (req, res) => {
-//     res.sendFile(__dirname, "./public/notes.html");
-// });
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}`);
